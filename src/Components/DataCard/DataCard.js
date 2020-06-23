b3da4e7b0ed023385be14cee5c7c8836ce97efae
Lucas Silva Beneti <LucasBeneti@Cesars-MacBook-Pro-2.local>
2020-06-23 17:46:32 -0300

    WIP

---
+++ src/Components/DataCard/DataCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./datacard.css";
+
+export const DataCard = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="dataTitle">Capacidade</div>
+        <div className="dataValue">58%</div>
+      </div>
+    </>
+  );
+};
